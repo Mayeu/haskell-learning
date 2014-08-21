fcce7c4dece89d8a502240b42ebc5cebcff684a3
Mayeu <m@6x9.fr>
2014-08-21 10:31:24 +0200

    Finish the modules part of learn you a haskell

--- learnyouahaskell/modules.hs
+++ learnyouahaskell/modules.hs
@@ -1,5 +1,7 @@
 import Data.List
 import Data.Char
+import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 search :: (Eq a) => [a] -> [a] -> Bool
 search needle haystack =
@@ -15,3 +17,51 @@ encode shift msg =
 
 decode :: Int -> String -> String
 decode shift msg = encode (negate shift) msg
+
+-- naive associated list
+phoneBook =
+    [("betty","555-2938")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ]
+
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey key [] = Nothing
+findKey key ((k,v):xs) = if key == k
+                            then Just v
+                            else findKey key xs
+
+-- with fold
+findKey' :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey' key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
+
+-- fromList from Map
+fromList' :: (Ord k) => [(k,v)] -> Map.Map k v
+fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty
+
+-- phone book with fromListWith
+phoneBook' =
+    [("betty","555-2938")
+    ,("betty","342-2492")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("patsy","943-2929")
+    ,("patsy","827-9162")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ,("penny","555-2111")
+    ]
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap = Map.fromListWith (\number1 number2 -> number1 ++ ", " ++ number2)
+
+phoneBookToMap' :: (Ord k) => [(k, a)] -> Map.Map k [a]
+phoneBookToMap' xs = Map.fromListWith (++) $ map (\(k,v) -> (k,[v])) xs
+
+-- Sets
+text1 = "I just had an anime dream. Anime... Reality... Are they so different?"
+text2 = "The old man left his garbage can out and now his trash is all over my lawn!"
