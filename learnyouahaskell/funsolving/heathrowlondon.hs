b82fea279dc764dd1cbd2927c348ac928a2ea22b
Mayeu <m@6x9.fr>
2014-08-22 16:46:08 +0200

    Add the heathrow to london resolution code

---
+++ learnyouahaskell/funsolving/heathrowlondon.hs
@@ -0,0 +1,53 @@
+import Data.List
+
+data Section    = Section { getA :: Int, getB :: Int, getC :: Int } deriving (Show)
+type RoadSystem = [Section]
+
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+
+-- main, read a road system from input, and calculate the optimal path
+main = do
+    contents <- getContents
+    let threes = groupsOf 3 (map read $ lines contents)
+        roadSystem = map (\[a,b,c] -> Section a b c) threes
+        path = optimalPath roadSystem
+        pathString = concat $ map (show . fst) path
+        pathPrice = sum $ map snd path
+    putStrLn $ "The best path to take is: " ++ pathString
+    putStrLn $ "The price is: " ++ show pathPrice
+
+-- The road system from heathrow to london
+heathrowToLondon :: RoadSystem
+heathrowToLondon = [Section 50 10 30, Section 5 90 20, Section 40 2 25, Section 10 8 0]
+
+-- Calculate the optimal next section for the two path
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+    let priceA = sum $ map snd pathA
+        priceB = sum $ map snd pathB
+        forwardPriceToA = priceA + a
+        crossPriceToA   = priceB + b + c
+        forwardPriceToB = priceB + b
+        crossPriceToB   = priceA + a + c
+        newPathToA = if forwardPriceToA <= crossPriceToA
+                        then (A,a):pathA
+                        else (C,c):(B,b):pathB
+        newPathToB = if forwardPriceToB <= crossPriceToB
+                        then (B,b):pathB
+                        else (C,c):(A,a):pathA
+    in  (newPathToA, newPathToB)
+
+-- Calculate the complete optimal path of a road system
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+    let (bestAPath, bestBPath) = foldl roadStep ([],[]) roadSystem
+    in  if sum (map snd bestAPath) <= sum (map snd bestBPath)
+           then reverse bestAPath
+           else reverse bestBPath
+
+-- group a list of element by three
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _  = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
