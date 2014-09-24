4d465b75e3da1e471f3a7ea040876b9bd1ea75e5
Mayeu <m@6x9.fr>
2014-09-24 16:47:25 +0200

    Second iteration of the Newton's square root method after coderiew.stackexchange.com

--- fp-principles-in-scala/course-exemple/week1.hs
+++ fp-principles-in-scala/course-exemple/week1.hs
@@ -1,12 +1,16 @@
 -- Implementing square root calculation with Newton's method
 
-sqrt' :: Double -> Double -> Double
-sqrt' x guess
-  | guessIsGoodEnough = guess
-  | otherwise         = sqrt' x improvedGuess
-  where
-    -- Test the precision of the guess
-    precision          = 0.001
-    guessIsGoodEnough  = abs (guess * guess - x) < precision * x
-    -- Improve our guessed square
-    improvedGuess      = (guess + x / guess) / 2
+import Data.List (find)
+
+refineSqrtGuess :: Double -> Double -> Maybe Double
+refineSqrtGuess x initial = find (acceptGuess x) (guesses x initial)
+
+acceptGuess :: Double -> Double -> Bool
+acceptGuess x guess = abs (guess * guess - x) < precision * x
+  where precision = 0.001
+
+guesses :: Double -> Double -> [Double]
+guesses x initial = iterate (next x) initial
+
+next :: Double -> Double -> Double
+next x guess = (guess + x / guess) / 2
