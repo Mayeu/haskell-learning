e787d4bd709afb7137f3af0eb24fe1d7b6dca897
Mayeu <m@6x9.fr>
2014-09-25 19:34:17 +0200

    Add part 1 of building a file hosting service

---
+++ school-of-haskell/file-hosting/main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Main where
+
+import Yesod
+
+data App = App
+instance Yesod App
+
+mkYesod "App" [parseRoutes|
+    / HomeR GET
+|]
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout $ do
+    setTitle "File Processor"
+    toWidget [whamlet|
+<h2>Previously submitted files
+<ul>
+    <li>readme.txt
+    <li>report.pdf
+    <li>music.wav
+|]
+
+main :: IO ()
+main = warp 3000 App
