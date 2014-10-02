ba13f47728b36e47fe4b8d1c24a0c6d4cc14927a
Mayeu <m@6x9.fr>
2014-10-02 15:06:13 +0200

    Modify all the author of the feed

--- test/rss/Main.hs
+++ test/rss/Main.hs
@@ -4,6 +4,8 @@ import Data.Maybe
 
 import Text.Feed.Import
 import Text.Feed.Query
+import Text.Feed.Types
+import Text.Feed.Constructor
 
 import System.Environment
 
@@ -11,6 +13,12 @@ main :: IO ()
 main = do
   (x:_) <- getArgs
   feed  <- parseFeedFromFile x
-  let items = getFeedItems feed
-      item = head items
-  putStrLn $ fromMaybe "Nothing. Lol" $ getItemDescription item
+  -- Change all authors of the feed to Mayeu
+  let myFeed = changeAuthors feed "Mayeu"
+  -- just print all author to check if it is working
+  mapM_ ((putStrLn . fromMaybe "Nothing") . getItemAuthor) $ getFeedItems myFeed
+
+-- Dumb function change all authors to a new one
+changeAuthors :: Feed -> String -> Feed
+changeAuthors feed author = withFeedItems (mapAuthor $ getFeedItems feed) feed
+  where mapAuthor = map (withItemAuthor author)
