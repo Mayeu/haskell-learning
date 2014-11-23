b9c264fe8db0ca727751345f59ff9af5f1ea2aff
Mayeu <m@6x9.fr>
2014-11-23 15:42:43 +0100

    Add exercices 3 and 4 from week 1 for cis194

--- cis194/week1/credit_card.hs
+++ cis194/week1/credit_card.hs
@@ -18,6 +18,21 @@ toDigits n
 
 -- Double every other number from the right of the list
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther []       = []
-doubleEveryOther [n]      = [n]
-doubleEveryOther (n:m:ns) = n : m*2 : doubleEveryOther ns
+doubleEveryOther ns = reverse $ mapEvery 2 (*2) (reverse ns)
+
+-- Map a function every n element
+-- Found here:
+-- http://stackoverflow.com/questions/26040420/in-haskell-what-is-the-most-common-way-to-apply-a-function-to-every-nth-element
+-- Basically this zip the list of element with a list of id function, where
+-- every Nth element are the f function. Thus, applying the f function every
+-- Nth element
+mapEvery :: Int -> (a -> a) -> [a] -> [a]
+mapEvery n f = zipWith ($) (drop 1 . cycle . take n $ f : repeat id)
+
+-- Sum all digit
+sumDigits :: [Integer] -> Integer
+sumDigits ns = sum $ map (sum . toDigits) ns
+
+-- Validate a credit card number
+validate :: Integer -> Bool
+validate n = ((sumDigits . doubleEveryOther . toDigits) n `rem` 10) == 0
