e814998195bdb359cc4ec220aec4f7a0480a8bc7
Mayeu <m@6x9.fr>
2014-12-12 12:32:32 +0100

    Add an exemple of exception handling

---
+++ school-of-haskell/conduit/exception.hs
@@ -0,0 +1,33 @@
+import System.IO
+import Data.Conduit
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Resource
+import qualified Data.Conduit.List as CL
+
+source =
+-- show Better with bracketP
+    bracketP
+        (openFile "test.txt" ReadMode)
+        (\handle -> putStrLn "Closing handle" >> hClose handle)
+        loop
+-- /show
+  where
+    loop handle = do
+        eof <- liftIO $ hIsEOF handle
+        if eof
+            then return ()
+            else do
+                c <- liftIO $ hGetChar handle
+                yield c
+                loop handle
+
+-- show An exception-throwing sink.
+exceptionalSink = do
+    c <- await
+    liftIO $ print c
+    error "This throws an exception"
+-- /show
+
+-- show We also need to call runResourceT
+main = runResourceT $ source $$ exceptionalSink
+-- /show
